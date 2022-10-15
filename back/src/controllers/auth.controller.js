c674b69be21367050603d5fbc7cdf868976522b7
Ezequiel <ezequielce490@gmail.com>
2022-10-15 00:19:44 -0300

    agregando verificacion de mail y contraseña en el login endpoint

--- back/src/controllers/auth.controller.js
+++ back/src/controllers/auth.controller.js
@@ -1,5 +1,25 @@
+import User from "../models/User.model.js";
+import jwt from "jsonwebtoken";
+
 export const login = async (req, res) => {
-  res.send("login route");
+  try {
+    const cuenta = await User.findOne({ where: { mail: req.body.mail } });
+    console.log(cuenta.contraseña);
+
+    if (!cuenta) {
+      return res.status(401).json({ msg: "Usuario o contraseña inválida" });
+    }
+
+    console.log(req.body.contraseña);
+    if (!(cuenta.contraseña === req.body.contraseña)) {
+      console.log("contraseña incorrecta");
+      return res.status(401).json({ msg: "Usuario o contraseña inválida" });
+    }
+
+    res.status(200).json({ msg: "logeado com sucesso" });
+  } catch (error) {
+    res.status(500).json({ msg: "Error al logearse" });
+  }
 };
 
 export const register = async (req, res) => {
