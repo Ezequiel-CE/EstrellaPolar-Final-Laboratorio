062e1745a02362f0b78f26c3d3a4860f15a45864
Ezequiel <ezequielce490@gmail.com>
2022-10-15 01:11:36 -0300

    agregando comentarios para entender el funcionamiento del enpoint login, falta bcrypt

--- back/src/controllers/auth.controller.js
+++ back/src/controllers/auth.controller.js
@@ -2,15 +2,15 @@ import User from "../models/User.model.js";
 import jwt from "jsonwebtoken";
 
 export const login = async (req, res) => {
+  //verifica que el mail exista
   try {
     const cuenta = await User.findOne({ where: { mail: req.body.mail } });
-    console.log(cuenta.contraseña);
 
     if (!cuenta) {
       return res.status(401).json({ msg: "Usuario o contraseña inválida" });
     }
 
-    console.log(req.body.contraseña);
+    //compara contraseña
     if (!(cuenta.contraseña === req.body.contraseña)) {
       console.log("contraseña incorrecta");
       return res.status(401).json({ msg: "Usuario o contraseña inválida" });
@@ -18,6 +18,7 @@ export const login = async (req, res) => {
 
     res.status(200).json({ msg: "logeado com sucesso" });
   } catch (error) {
+    //error en el endpoint
     res.status(500).json({ msg: "Error al logearse" });
   }
 };
