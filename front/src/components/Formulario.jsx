0103b6893dfeb334079dedaf436191f317181665
PGZeeD <ezeadh2690@gmail.com>
2022-10-15 15:56:59 -0300

    corrección de font , elimino archivos innecesarios(Formulario.css,Amatic SC-Regular.ttf). to do:"reparar logo-avion(no renderiza)"

--- front/src/components/Formulario.jsx
+++ front/src/components/Formulario.jsx
@@ -1,20 +1,15 @@
 /** @format */
 
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import CardMedia from "@mui/material/CardMedia";
 import Card from "@mui/material/Card";
-import { createTheme, responsiveFontSizes, ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-
+import AmaticSCBold from "../assets/AmaticSCBold.ttf";
 const theme = createTheme({
 	palette: {
 		orange: {
@@ -23,24 +18,23 @@ const theme = createTheme({
 		},
 	},
 	typography: {
-		fontFamily: [
-			"-apple-system",
-			"BlinkMacSystemFont",
-			'"Segoe UI"',
-			"Roboto",
-			'"Helvetica Neue"',
-			"Arial",
-			"sans-serif",
-			'"Apple Color Emoji"',
-			'"Segoe UI Emoji"',
-			'"Segoe UI Symbol"',
-			'"Amatica SC"',
-			'"Sansita One"',
-		].join(","),
+		fontFamily: "Amatic SC, Arial",
+	},
+	components: {
+		MuiCssBaseline: {
+			styleOverrides: `
+        @font-face {
+          font-family: 'Amatic SC';
+          font-style: bold;
+          font-display: swap;
+          font-weight: 400;
+          src: local('Amatic SC'), local('Amatic SC-Bold'), url(${AmaticSCBold}) format('ttf');
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
+		},
 	},
 });
-let Rtheme = createTheme();
-Rtheme = responsiveFontSizes(Rtheme);
 
 export default function Formulario() {
 	return (
@@ -48,7 +42,7 @@ export default function Formulario() {
 			width={"542px"}
 			height={"660px"}
 			sx={{
-				px: "71px",
+				p: "40px 71px 20px 71px",
 				borderRadius: "20px",
 				ml: "48px",
 				backgroundColor: "#F3F3F3",
@@ -62,18 +56,24 @@ export default function Formulario() {
 				justifyContent="center"
 				alignItems="center"
 			>
-				<ThemeProvider theme={Rtheme}>
+				<ThemeProvider theme={theme}>
 					<Typography variant="h5">Welcome</Typography>
 					<Typography variant="h5">to</Typography>
-					<Typography variant="h4" fontFamily="Amatica SC">
+					<Typography
+						variant="h4"
+						sx={{
+							fontFamily: "Amatic SC",
+							fontWeight: "bold",
+						}}
+					>
 						Unstick Airlines
 					</Typography>
 				</ThemeProvider>
 			</Grid>
 			<CardMedia
 				component="img"
-				height="140"
-				image="/front/src/assets/login-logo.jpg"
+				style={{ height: 100, paddingTop: "15px" }}
+				image="assets/login-logo.jpg"
 				alt="Plain Logo"
 			/>
 			<Grid
