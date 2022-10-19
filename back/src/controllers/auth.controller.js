347f6d57572a8bfb5537230bc8187ecc3e3be94f
Ezequiel <ezequielce490@gmail.com>
2022-10-18 23:22:30 -0300

    feat(Cuenta.model.js/auth.controller.js): modelado de Cuenta y modificacion en el contrallador de auth

--- back/src/controllers/auth.controller.js
+++ back/src/controllers/auth.controller.js
@@ -1,10 +1,10 @@
 // import jwt from 'jsonwebtoken';
-import User from '../models/User.model.js';
+import Cuenta from '../models/Cuenta.model.js';
 
 export const login = async (req, res) => {
   // verifica que el mail exista
   try {
-    const cuenta = await User.findOne({ where: { mail: req.body.mail } });
+    const cuenta = await Cuenta.findOne({ where: { email: req.body.email } });
 
     if (!cuenta) {
       return res.status(401).json({ msg: 'Usuario o contraseña inválida' });
@@ -18,6 +18,7 @@ export const login = async (req, res) => {
 
     return res.status(200).json({ msg: 'logeado com sucesso' });
   } catch (error) {
+    console.log(error);
     // error en el endpoint
     return res.status(500).json({ msg: 'Error al logearse' });
   }
---
+++ back/src/models/Cuenta.model.js
@@ -0,0 +1,19 @@
+import { DataTypes } from 'sequelize';
+import db from '../configs/database.js';
+
+const Cuenta = db.define(
+  'cuenta',
+  {
+    id: {
+      field: 'id',
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: { field: 'email', type: DataTypes.STRING, allowNull: false },
+    password: { field: 'password', type: DataTypes.STRING, allowNull: false },
+  },
+  { tableName: 'cuenta', timestamps: false },
+);
+
+export default Cuenta;
