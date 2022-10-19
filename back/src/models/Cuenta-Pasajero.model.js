b2ebff217fded6f44db6497722590da13a679011
Ezequiel <ezequielce490@gmail.com>
2022-10-19 00:21:23 -0300

    Agregando la accion que hara en delete y en update

--- back/src/models/Cuenta-Pasajero.model.js
+++ back/src/models/Cuenta-Pasajero.model.js
@@ -19,13 +19,20 @@ const CuentaPasajero = db.define(
       references: {
         model: Cuenta,
         key: 'id_cuenta',
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
       },
     },
     id_pasajero: {
       field: 'id_pasajero',
       type: DataTypes.INTEGER,
       allowNull: false,
-      references: { model: Pasajero, key: 'id_pasajero' },
+      references: {
+        model: Pasajero,
+        key: 'id_pasajero',
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
     },
     puntos: { field: 'puntos', type: DataTypes.INTEGER, allowNull: false },
   },
