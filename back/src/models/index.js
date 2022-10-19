b411141521e65367879718c0e41f9af28b094bbb
Joy(Marcelo) <marce.giamp@gmail.com>
2022-10-19 00:53:31 -0300

    fix: actualizacion de los patch de los import de index.js

--- back/src/models/index.js
+++ back/src/models/index.js
@@ -1,6 +1,6 @@
-import CuentaPasajero from './Cuenta-Pasajero.model.js';
-import Cuenta from './Cuenta.model.js';
-import Pasajero from './Pasajero.model.js';
+import CuentaPasajero from './cuenta-pasajero.model.js';
+import Cuenta from './cuenta.model.js';
+import Pasajero from './pasajero.model.js';
 
 // Relaciones de cuenta_pasajero
 Cuenta.belongsToMany(Pasajero, { through: CuentaPasajero });
