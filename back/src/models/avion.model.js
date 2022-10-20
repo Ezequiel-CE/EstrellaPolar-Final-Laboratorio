41376414deaa9287c74c10184905aad22f389f92
Marcos_Wamba <marcos_w_03@hotmail.com>
2022-10-19 21:39:41 -0300

    se agrego el modelo avion y su relacion avion-vuelo

---
+++ back/src/models/avion.model.js
@@ -0,0 +1,18 @@
+import { DataTypes } from 'sequelize';
+import db from '../configs/database.js';
+
+const Avion = db.define(
+  'avion',
+  {
+    id: {
+      field: 'id',
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    aerolinea: { field: 'aerolinea', type: DataTypes.STRING(60), allowNull: false },
+    capacidad: { field: 'capacidad', type: DataTypes.INTEGER(60), allowNull: false },
+  },
+  { tableName: 'avion', timestamps: false },
+);
+export default Vuelo;
