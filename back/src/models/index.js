0497c0c86e7bda35739f358225462a2b70dd3fb9
Joy(Marcelo) <marce.giamp@gmail.com>
2022-10-28 00:17:40 -0300

    style: cambiando la notacion kebab a snake en los modelos

--- back/src/models/index.js
+++ back/src/models/index.js
@@ -1,10 +1,10 @@
-import AvionVuelo from './avion-vuelo.js';
+import AvionVuelo from './avion_vuelo.js';
 import Avion from './avion.model.js';
-import CuentaPasajero from './cuenta-pasajero.model.js';
+import CuentaPasajero from './cuenta_pasajero.model.js';
 import Cuenta from './cuenta.model.js';
-import PasajeVuelo from './pasaje-vuelo.model.js';
+import PasajeVuelo from './pasaje_vuelo.model.js';
 import Pasaje from './pasaje.model.js';
-import PasajeroCompraPasaje from './pasajero-compra-pasaje.model.js';
+import PasajeroCompraPasaje from './pasajero_compra_pasaje.model.js';
 import Pasajero from './pasajero.model.js';
 import Vuelo from './vuelo.model.js';
 
