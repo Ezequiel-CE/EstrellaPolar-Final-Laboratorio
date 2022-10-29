8584343d934f915bb8b07514fad305b50b207917
Ezequiel <ezeadh2690@gmail.com>
2022-10-29 14:39:35 -0300

    arreglos de modularizacion

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import Login from './Page/Login';
+import Login from './pages/login/Login';
+import './App.css';
 
 function App() {
   return (
-    <div className="app">
+    <div className="background">
       <Login />
     </div>
   );
--- front/src/pages/login/Login.jsx
+++ front/src/pages/login/Login.jsx
@@ -1,12 +1,26 @@
-import '../index.css';
-import Formulario from './components/Formulario.jsx';
+import React from 'react';
+import Card from '@mui/material/Card';
+import FormularioLogin from './components/Formulario.login';
+import HeaderFormulario from './components/Header.login';
 
-const Login = () => {
+function Login() {
   return (
-    <div className="background">
-      <Formulario />
+    <div>
+      <Card
+        sx={{
+          px: '35px',
+          borderRadius: '30px',
+          ml: '50px',
+          backgroundColor: '#F3F3F3',
+          boxShadow:
+            '200px 132px 96px rgba(0, 0, 0, 0.01), 113px 74px 81px rgba(0, 0, 0, 0.05), 50px 33px 60px rgba(0, 0, 0, 0.09), 13px 8px 33px rgba(0, 0, 0, 0.1), 0px 0px 0px rgba(0, 0, 0, 0.1);',
+        }}
+      >
+        <HeaderFormulario />
+        <FormularioLogin />
+      </Card>
     </div>
   );
-};
+}
 
 export default Login;
