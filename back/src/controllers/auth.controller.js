52a5e01dc6da0a7207d83af7e4b345d811696465
Ezequiel <ezequielce490@gmail.com>
2022-10-28 23:22:29 -0300

    fix:move the logic of the controllers to the services

--- back/src/controllers/auth.controller.js
+++ back/src/controllers/auth.controller.js
@@ -1,64 +1,33 @@
 // import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import { encontrarCuenta, crearCuenta } from '../services/cuenta.services.js';
+import { LogearCuenta, registrarCuenta } from '../services/cuenta.services.js';
 import cuentaValidation from '../lib/validation.js';
 
 export const register = async (req, res) => {
   // validation
   const { error, value } = cuentaValidation(req.body);
 
-  if (error) {
-    throw new Error('datos no coinciden con lo que se solicita');
-  }
-
   try {
-    // encuentra la cuenta
-    encontrarCuenta(value.email);
+    if (error) {
+      throw new Error('mal request body');
+    }
 
-    // hashea password y crea cuenta
-    const hashedPassword = await bcrypt.hash(value.password, 8);
-
-    await crearCuenta(value.email, hashedPassword);
-
+    await registrarCuenta(value);
     res.status(200).json({ mensaje: 'Cuenta creada con exito' });
   } catch (err) {
-    res.status(403).json({ mensaje: err });
+    res.status(403).json({ mensaje: err.message });
   }
 };
 
 export const login = async (req, res) => {
   // validation
-  const { error, value } = cuentaValidation(req.body);
-
-  if (error) {
-    throw new Error('datos no coinciden con lo que se solicita');
-  }
-
   try {
-    // verifica que el mail exista
-    const cuenta = await encontrarCuenta(value.email);
+    const { value } = cuentaValidation(req.body);
 
-    if (!cuenta) {
-      throw new Error('Usuario o contraseña inválida');
-    }
+    const token = await LogearCuenta(value);
 
-    // compara cuentas
-    const comparedPassword = await bcrypt.compare(value.password, cuenta.password);
-
-    if (!comparedPassword) {
-      throw new Error('Usuario o contraseña inválida');
-    }
-
-    // crea token
-
-    const token = jwt.sign({ cuenta: value.email }, process.env.JWT_SECRET, {
-      expiresIn: '2h',
-    });
-
-    return res.status(200).json({ mensaje: 'logeado con exito', token });
+    res.status(200).json({ mensaje: 'logeado con exito', token });
   } catch (err) {
     // error en el endpoint
-    throw new Error('error en el endpoint');
+    res.status(403).json({ mensaje: err.message });
   }
 };
--- back/src/services/cuenta.services.js
+++ back/src/services/cuenta.services.js
@@ -1,13 +1,40 @@
 /* eslint-disable implicit-arrow-linebreak */
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import Cuenta from '../models/cuenta.model.js';
 
-export const encontrarCuenta = (email) => {
-  const cuenta = Cuenta.findOne({ where: { email } });
+export const registrarCuenta = async ({ email, password }) => {
+  const cuenta = await Cuenta.findOne({ where: { email } });
+
   if (cuenta) throw new Error('Cuenta existe');
-  return cuenta;
-};
-export const crearCuenta = (email, password) =>
-  Cuenta.create({
+
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  await Cuenta.create({
     email,
-    password,
+    password: hashedPassword,
   });
+};
+
+export const LogearCuenta = async ({ email, password }) => {
+  // encuentra cuenta
+  const cuenta = await Cuenta.findOne({ where: { email } });
+
+  if (!cuenta) {
+    throw new Error('Usuario o contraseña inválida');
+  }
+
+  // compara cuentas
+  const comparedPassword = await bcrypt.compare(password, cuenta.password);
+
+  if (!comparedPassword) {
+    throw new Error('Usuario o contraseña inválida');
+  }
+
+  // crea token
+
+  const token = jwt.sign({ cuenta: email }, process.env.JWT_SECRET, {
+    expiresIn: '2h',
+  });
+  return token;
+};
