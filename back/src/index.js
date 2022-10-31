21e3f1eb1d55b7f1ff3955b7b2f09f7ef1424a12
Joy(Marcelo) <marce.giamp@gmail.com>
2022-10-31 00:17:36 -0300

    feat: Agregado funcionalidad de Agregar, editar, ver y eliminar vuelos

--- back/src/index.js
+++ back/src/index.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import db from './configs/database.js';
 
 import authRouter from './routes/auth.route.js';
+import routerVuelo from './routes/vuelo.route.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,12 +12,15 @@ app.use(express.json());
 app.use(morgan('tiny'));
 
 app.use('/API/auth', authRouter);
+app.use('/API/itinerario', routerVuelo);
 
 app.get('/', (_, res) => {
   res.send('API GO');
 });
 
-app.use('*', (_, res) => res.status(404).send('<h1>OPS! the endpoint does not exist :(</h1>'));
+app.use('*', (_, res) => {
+  res.status(404).send('<h1>OPS! the endpoint does not exist :(</h1>');
+});
 
 app.listen(port, async () => {
   try {
---
+++ back/src/services/vuelo.service.js
@@ -0,0 +1,68 @@
+import Vuelo from '../models/vuelo.model.js';
+import { BodyVuelo, editarVuelo } from '../schemas/vuelo.schema.js';
+
+const postVuelo = (body) => {
+  const { error, value } = BodyVuelo(body);
+
+  const { tarifa, destino, origen } = value;
+
+  if (error) {
+    throw new Error(error);
+  }
+
+  return Vuelo.create({
+    tarifa,
+    destino,
+    origen,
+  });
+};
+
+const patchVuelo = async (vuelo) => {
+  const { error, value } = editarVuelo(vuelo.body);
+
+  if (error) {
+    throw new Error(error);
+  }
+
+  const vueloEditado = await Vuelo.update(
+    { ...value },
+    {
+      where: {
+        id: vuelo.id,
+      },
+    },
+  );
+  return vueloEditado;
+};
+
+const getVuelo = async (id) => {
+  const vuelo = await Vuelo.findOne({ where: { id } });
+  if (!vuelo) {
+    throw new Error('No se encontro vuelo');
+  }
+  return vuelo;
+};
+
+const deleteVuelo = async (id) => {
+  const vuelo = await Vuelo.destroy({ where: { id } });
+  if (!vuelo) {
+    throw new Error('No se encontro vuelo');
+  }
+  return vuelo;
+};
+
+const getVuelos = async () => {
+  const vuelos = await Vuelo.findAll();
+  if (vuelos.length === 0) throw new Error('No se encontro vuelos');
+  return vuelos;
+};
+
+const servicio = {
+  postVuelo,
+  getVuelo,
+  getVuelos,
+  patchVuelo,
+  deleteVuelo,
+};
+
+export default servicio;
